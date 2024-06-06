c3e48df05a21cd03ed0f8cfb3b603da8d3920e56
elishamutang <elishamutang@outlook.com>
2024-06-07 00:03:32 +0800

    Imported mergeSort algorithm from previous project. Finished buildTree function and insert method for BT.

--- src/binarySearchTree.js
+++ src/binarySearchTree.js
@@ -1,3 +1,6 @@
+import mergeSort from 'mergesort-by-eli'
+import { prettyPrint } from './prettyPrint.js'
+
 function node(data) {
     return {
         data,
@@ -7,13 +10,62 @@ function node(data) {
 }
 
 function tree(array) {
-    let root
+    return {
+        root: buildTree(array),
+
+        insert(value) {
+            let newNode = node(value)
+            let tmp = rootNode.root
+
+            while (tmp.left !== null || tmp.right !== null) {
+                if (newNode.data < tmp.data) {
+                    tmp = tmp.left
+                } else {
+                    tmp = tmp.right
+                }
+            }
+
+            if (newNode.data < tmp.data) {
+                tmp.left = newNode
+            } else {
+                tmp.right = newNode
+            }
+        },
+
+        deleteItem(value) {},
+
+        find(value) {},
+    }
 }
 
+// Returns root element (level-0) of array.
 function buildTree(array) {
-    // BSTs work only for a sorted array, hence we will have to do a merge sort on the passed array.
+    // Base case
+    let start = 0
+    let end = array.length - 1
+
+    if (start > end) {
+        return null
+    }
+
+    let midpoint = parseInt((array.length - 1) / 2)
+    let newNode = node(array[midpoint])
+
+    newNode.left = buildTree(array.slice(0, midpoint))
+    newNode.right = buildTree(array.slice(midpoint + 1))
+
+    return newNode
 }
 
-const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+// BSTs work only for a sorted array, hence we will have to do a merge sort on the passed array and remove any duplicates.
+const unsortedArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324]
+const sortedArr = mergeSort(unsortedArr)
 
-console.log(buildTree(testArr))
+const noDuplicates = sortedArr.filter((elem, idx) => {
+    return sortedArr.indexOf(elem) === idx
+})
+
+const rootNode = tree(noDuplicates)
+
+rootNode.insert(10)
+prettyPrint(rootNode.root)
